8e091821add2cdf1021675e0329ce866a5f8ff95
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2022-04-13 22:07:08 +0200

    add Can_get_all_java_files_from_folders

---
+++ HomeworkCheckerLib/DirectoryService.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace HomeworkCheckerLib
+{
+  internal class DirectoryService
+  {
+    internal interface IFileEnumerator
+    {
+      IEnumerable<string> GetFilesInFolderRecursivly(string folder, string extension);
+    }
+
+    IFileEnumerator fileEnumerator;
+
+    public DirectoryService(IFileEnumerator fileEnumerator)
+    {
+      this.fileEnumerator = fileEnumerator;
+    }
+
+    internal IEnumerable<string> GetAllHomeWorkFolders(string folder)
+    {
+      var files = fileEnumerator.GetFilesInFolderRecursivly(folder, "*.java");
+      return files.Select(f => Path.GetDirectoryName(f)!);
+    }
+  }
+}
---
+++ HomeworkCheckerLibTest/TestDirectoryService.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using HomeworkCheckerLib;
+using System.Collections.Generic;
+using Xunit;
+
+namespace HomeworkCheckerLibTest
+{
+  public class TestDirectoryService
+  {
+    [Fact]
+    public void Can_get_all_java_files_from_folders()
+    {
+      var folder = "arbitraryFolderName";
+      var sut = new DirectoryService(new TestFileEnumerator());
+
+      var files = sut.GetAllHomeWorkFolders(folder);
+
+      files.Should().Equal(new List<string> { "folderA", "folderB" });
+    }
+
+    class TestFileEnumerator : DirectoryService.IFileEnumerator
+    {
+      public IEnumerable<string> GetFilesInFolderRecursivly(string folder, string extension)
+      {
+        extension.Should().Be("*.java");
+        return new List<string> { @"folderA\aaa.java", @"folderB\bbb.java" };
+      }
+    }
+  }
+}
