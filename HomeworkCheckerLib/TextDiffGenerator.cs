0e5ff8b90c4be8e460e040e249e557a2b92950b4
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2022-04-18 13:02:25 +0200

    add TextDiff generator

---
+++ HomeworkCheckerLib/TextDiffGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkCheckerLib
+{
+  internal class TextDiffGenerator
+  {
+    public record Difference(List<DiffMatchPatch.Diff> Diffs);
+
+    internal static Difference GenerateDiff(string oldText, string newText)
+    {
+      var dmp = new DiffMatchPatch.diff_match_patch();
+      var diffs = dmp.diff_main(oldText, newText, false);
+      dmp.diff_cleanupSemantic(diffs);
+
+      return new Difference(diffs);
+    }
+  }
+}
---
+++ HomeworkCheckerLibTest/TestTextDifferences.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using HomeworkCheckerLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HomeworkCheckerLibTest
+{
+  public class TestTextDifferences
+  {
+    [Fact]
+    public void Can_generate_text_differences()
+    {
+      var oldText = "Text";
+      var newText = "TextX";
+
+      var diff = TextDiffGenerator.GenerateDiff(oldText, newText);
+
+      diff.Diffs.Should().Contain(new DiffMatchPatch.Diff(DiffMatchPatch.Operation.INSERT, "X"));
+    }
+  }
+}
