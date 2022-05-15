561175948863f9b7cd742d2fd263566ab08a89bb
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2022-05-15 12:38:57 +0200

    use -g to compile with javac

--- HomeworkCheckerLib/JavaCompiler.cs
+++ HomeworkCheckerLib/JavaCompiler.cs
@@ -14,7 +14,7 @@
       var workingDirectory = Path.GetDirectoryName(javaFilePath);
       var javaFile = Path.GetFileName(javaFilePath);
 
-      var executeResult = appExecuter.Execute("javac", workingDirectory!, $"-Xlint -encoding UTF8 \"{javaFile}\"");
+      var executeResult = appExecuter.Execute("javac", workingDirectory!, $"-g -Xlint -encoding UTF8 \"{javaFile}\"");
 
       return new(executeResult.ExitCode == 0, executeResult.Output);
     }
