acb4086c04e594d75f6513838c4c08ac978bf2f4
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2022-06-03 10:07:41 +0200

    refactor main into separate methods

--- HomeworkChecker/Program.cs
+++ HomeworkChecker/Program.cs
@@ -11,34 +11,49 @@
         return;
       }
 
-      var homeworkChecker = new HomeworkCheckerLib.HomeworkChecker();
       if (args.Length == 1)
       {
-        var analysisResult = homeworkChecker.ProcessMaster(args[0]);
-        homeworkChecker.WriteAnalysisToMarkdownFile(analysisResult);
-        if (homeworkChecker.StartVSCodeWithFolder(args[0]) == 0)
-          homeworkChecker.CleanUpMarkdownFiles(analysisResult);
+        ProcessMaster(args[0]);
       }
       else
       {
         if (args[0] == "--evaluateOnly")
         {
-          var percentageAdder = new HomeworkCheckerLib.PercentageAdder();
-          percentageAdder.ProcessPercentages(args[1]);
+          EvaluatePercentage(args[1]);
         }
         else
         {
-          var analysisResult = homeworkChecker.ProcessHomework(args[0], args[1]);
-          homeworkChecker.WriteAnalysisToMarkdownFile(analysisResult);
-
-          if (homeworkChecker.StartVSCodeWithFolder(args[1]) == 0)
-          {
-            homeworkChecker.CleanUpMarkdownFiles(analysisResult);
-            var percentageAdder = new HomeworkCheckerLib.PercentageAdder();
-            percentageAdder.ProcessPercentages(analysisResult.SubmissionBaseFolder);
-          }
+          ProcessHomeworks(args[0], args[1]);
         }
       }
+    }
+
+    private static void ProcessHomeworks(string masterFolder, string homeworkFolder)
+    {
+      var homeworkChecker = new HomeworkCheckerLib.HomeworkChecker();
+      var analysisResult = homeworkChecker.ProcessHomework(masterFolder, homeworkFolder);
+      homeworkChecker.WriteAnalysisToMarkdownFile(analysisResult);
+
+      if (homeworkChecker.StartVSCodeWithFolder(homeworkFolder) == 0)
+      {
+        homeworkChecker.CleanUpMarkdownFiles(analysisResult);
+        EvaluatePercentage(analysisResult.SubmissionBaseFolder);
+      }
+    }
+
+    private static void EvaluatePercentage(string homeworkFolder)
+    {
+      var percentageAdder = new HomeworkCheckerLib.PercentageAdder();
+      percentageAdder.ProcessPercentages(homeworkFolder);
+    }
+
+    private static void ProcessMaster(string masterFolder)
+    {
+      var homeworkChecker = new HomeworkCheckerLib.HomeworkChecker();
+      var analysisResult = homeworkChecker.ProcessMaster(masterFolder);
+      homeworkChecker.WriteAnalysisToMarkdownFile(analysisResult);
+      if (homeworkChecker.StartVSCodeWithFolder(masterFolder) == 0)
+        homeworkChecker.CleanUpMarkdownFiles(analysisResult);
     }
   }
 }
